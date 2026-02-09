dbb9edba80a821cefd65ea440e2e0215a988c123
ShayShN <shayshn1234@gmail.com>
2026-02-09 17:55:48 +0200

    d

--- index.js
+++ index.js
@@ -1,87 +1,141 @@
-// import cors from 'cors'
 
 const root = document.getElementById('root')
+const main = document.getElementById('main')
 
+const getData = async function () {
+    const local = localStorage.getItem("data")
+    if (!local) {
+        const response = await fetch("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=955a96fed1e54748bd8f9b6e9b9861c9")
+        const data = await response.json()
+        console.log(data);
 
-async function getData() {
-    const response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2026-01-08&sortBy=publishedAt&apiKey=955a96fed1e54748bd8f9b6e9b9861c9")
-    const data = await response.json()
-    // console.log(data);
-    return (await data.articles)
-
-    // console.log(await data.articles);
-
-
-    // console.log( response.json(data));
-    // console.log();
-
-
+        localStorage.setItem("data", JSON.stringify(data.articles));
+        return data.articles
+    }
+    return JSON.parse(localStorage.getItem("data"));
 }
-// getData()
 
-
-
-const createNav = async function () {
-
+const createNav = function () {
     const nav = document.createElement('nav')
 
     const home = document.createElement('a')
     const create = document.createElement('a')
-    const expanded = document.createElement('a')
 
     home.href = '/home'
     create.href = '/create'
-    expanded.href = '/expanded'
 
     home.textContent = "Home"
     create.textContent = "Create"
-    expanded.textContent = "Expanded"
 
-
-    nav.append(home, create, expanded)
+    nav.append(home, create)
     nav.classList.add('link')
+    root.appendChild(nav)
+}
 
+const clickAction = async function () {
 
     document.body.addEventListener('click', async (event) => {
         event.preventDefault()
         const target = event.target
+
+
         if (target) {
-            console.log(target.textContent);
+            console.log(target);
+
+            // console.log(target.textContent);
+
             if (target.textContent == "Home") {
-                const articles = await getData()
-                articles.forEach(art => {
-                    let news = document.createElement('div')
-                    let author = document.createElement('author')
-                    let title = document.createElement('p')
-                    let urlToImage = document.createElement('img')
-
-                    author.textContent = art.author
-                    title.textContent = art.title
-                    urlToImage.src = art.urlToImage
-
-                    console.log(author);
-                    
-
-                    news.append(author, title, urlToImage)
-                    news.classList.add('news')
-                    root.appendChild(news)
-
-                });
-                console.log(articles);
-
+                await createAllCards()
             }
 
             if (target.textContent == "Create") {
+
             }
 
-            if (target.textContent == "Expanded") {
+            if (target.classList.contains('news')) {
+               
+                await extend(target)
+
+            } else if (target.parentElement.classList.contains('news')) {
+    
+                await extend(target.parentElement)
             }
         }
-    })
-    root.appendChild(nav)
+    });
+}
+
+const extend = async (card) => {
+
+    console.log(card);
+
+    const data = await getData()
+    console.log(data);
+
+    const cardDetails = data[card.id];
+    console.log(cardDetails);
+
+    main.innerHTML = "";
+
+    let extended = document.createElement('div')
+
+    let author = document.createElement('p')
+    let title = document.createElement('h1')
+    let urlImage = document.createElement('img')
+    let description = document.createElement('p')
+    let content = document.createElement('p')
+
+    author.textContent = cardDetails.author
+    title.textContent = cardDetails.title
+    urlImage.src = cardDetails.urlToImage
+    description.textContent = cardDetails.description
+    content.textContent = cardDetails.content
+
+    extended.append(author, title, urlImage, description, content)
+    
+    extended.className = 'extended'
+    // main.id = 'extended'
+    main.appendChild(extended)
+}
+
+const createAllCards = async () => {
+    main.innerHTML = "";
+    const articles = await getData()
+    articles.forEach((art, i) => {
+        let news = document.createElement('button')
+
+
+        let author = document.createElement('p')
+        let title = document.createElement('h1')
+        let urlImage = document.createElement('img')
+
+        author.textContent = art.author
+        title.textContent = art.title
+        urlImage.src = art.urlToImage
+
+        news.append(author, title, urlImage)
+        news.classList.add('news')
+        news.id = i;
+        main.appendChild(news)
+    });
+}
+
+const createBlock = async () => {
+    main.innerHTML = "";
+    
+    let header = document.createElement('h1')
+    let title = document.createElement('input')
+    let file = document.createElement('input')
+    let description = document.createElement('p')
+
+    header.textContent = 
 
 }
+
+clickAction()
 createNav()
+createAllCards();
+
+
 
 
 
